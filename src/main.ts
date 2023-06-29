d19827d4a1e890aff9b183d53343c69a3471229e
Bùi Ngọc Thành <thanhbn@tokyotechlab.com>
2023-06-29 13:16:18 +0700

    config base project

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appConfig } from './common/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationExceptionFilter } from './common/filters';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
-    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalFilters(new ValidationExceptionFilter());
     await app.listen(appConfig().port);
     console.log('app listening on port ' + appConfig().port);
 }
